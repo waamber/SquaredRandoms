70d02ac7d1518d99ba24ec0a48a70d9b86b5f449
waamber <amberastuart@aol.com>
2018-01-21 15:21:23 -0600

    setup

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNum = random.Next(1, 50);
+
+            Console.WriteLine(randomNum);
+            Console.ReadKey();
+
+        }
+    }
+}
